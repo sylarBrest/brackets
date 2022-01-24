be8f81918556c70903014adf0506e1fee634c486
sylarBrest <aostrovsky87@gmail.com>
2022-01-24 17:00:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = bracketsConfig.join(',').split(',').join('');
+  let brackets = [];
+  for (let i = 0; i < res.length; i += 2) {
+    brackets.push(res.slice(i, i + 2));
+  }
+  let base = true;
+  let restLength = str.length;
+  while (base) {
+    restLength = str.length;
+    for (let i = 0; i < brackets.length; i++) {
+      str = str.replace(brackets[i],'');
+    }
+    if (restLength === str.length) {
+      base = false;
+    }
+  }
+  return (str.length === 0);
 }
